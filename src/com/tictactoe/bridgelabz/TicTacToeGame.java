c9fcc3e43c53d5e69915aac91efb1389f3dae5d0
amarprajapati99 <amarprajapati99@gmail.com>
2021-03-31 19:06:35 +0530

    Welcome to the tic tac toe game

---
+++ src/com/tictactoe/bridgelabz/TicTacToeGame.java
@@ -0,0 +1,15 @@
+package com.tictactoe.bridgelabz;
+
+import java.util.Scanner;
+
+/**
+ * print welcome message
+ */
+
+public class TicTacToeGame {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the tic toe game");
+
+	}
+
+}
