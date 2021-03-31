f7bfd2d3024a7dc378b38f0e601909102f1ed6da
amarprajapati99 <amarprajapati99@gmail.com>
2021-03-31 19:15:50 +0530

    Create a empty tic tac toe board

--- src/com/tictactoe/bridgelabz/TicTacToeGame.java
+++ src/com/tictactoe/bridgelabz/TicTacToeGame.java
@@ -4,11 +4,22 @@ import java.util.Scanner;
 
 /**
  * print welcome message
+ * 
+ * @Description- create a empty tic tac toe toe board
  */
 
 public class TicTacToeGame {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the tic toe game");
+		Scanner scanner = new Scanner(System.in);
+
+		char[][] gameBoard = { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' } };
+
+		System.out.println(gameBoard[0][0] + "|" + gameBoard[0][1] + "|" + gameBoard[0][2]);
+		System.out.println("-+-+-");
+		System.out.println(gameBoard[1][0] + "|" + gameBoard[1][1] + "|" + gameBoard[1][2]);
+		System.out.println("-+-+-");
+		System.out.println(gameBoard[2][0] + "|" + gameBoard[2][1] + "|" + gameBoard[2][2]);
 
 	}
 
